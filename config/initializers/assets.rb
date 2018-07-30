88297b68d155467e5e3dd59c39539674ef81a6be
imaginnection <imaginnection@gmail.com>
2018-07-30 03:53:01 +0000

    amend locals, and move tour.js file.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( vendor/* firebase/* three/* )
+Rails.application.config.assets.precompile += %w( vendor/* common/* firebase/* three/* )
