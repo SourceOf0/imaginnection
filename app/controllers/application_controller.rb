115edd468ac022bcee27c3b5db4d1fe218b54fcb
imaginnection <imaginnection@gmail.com>
2018-08-29 04:49:48 +0000

    add basic authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :site_http_basic_authenticate_with if ENV['USE_BASIC'] == 'true'
 
   # deviseでのログイン認証
   before_action :authenticate_user!
@@ -11,6 +12,13 @@ class ApplicationController < ActionController::Base
   # deviseのstring parameter
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def site_http_basic_authenticate_with
+    authenticate_or_request_with_http_basic("Application") do |name, password|
+      name == ENV['BASIC_USERNAME'] && password == ENV['BASIC_PASSWORD']
+    end
+  end
+  
   protected
   
   # deviseのstring parameter
