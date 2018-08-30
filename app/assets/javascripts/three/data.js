f83ca314590ff460d19adb733e483d05ffc89c88
imaginnection <imaginnection@gmail.com>
2018-08-30 11:33:33 +0000

    amend edge line style

--- app/assets/javascripts/three/data.js
+++ app/assets/javascripts/three/data.js
@@ -237,7 +237,7 @@ imaginnection.three.Edge = {
 		
 		var color = imaginnection.threeData.normalColor;
 		var geometry = new THREE.BufferGeometry().setFromPoints( [from_node.particle.position, to_node.particle.position] );
-		var line = new THREE.Line( geometry, new THREE.LineDashedMaterial( { color: color, opacity: 0.5, dashSize: 40, gapSize: 10, linewidth: imaginnection.threeData.edgeDefaultLineWidth } ) );
+		var line = new THREE.Line( geometry, new THREE.LineDashedMaterial( { color: color, opacity: 0.5, dashSize: 40, gapSize: 3, linewidth: imaginnection.threeData.edgeDefaultLineWidth } ) );
 		
 		line.name = from_node.name + " -> " + to_node.name;
 		line.material.linecap = "butt";
@@ -300,7 +300,7 @@ imaginnection.three.Edge = {
 				this.is_owner = false;
 				var color = imaginnection.threeData.normalColor;
 				line.material.color.set(color);
-				line.material.gapSize = 10;
+				line.material.gapSize = 3;
 				from_node.resetOwner( to_node );
 			},
 		};
