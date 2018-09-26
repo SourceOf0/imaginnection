93dafa893ed0b540b37f79dfb620b7a71c5c9f2f
imaginnection <imaginnection@gmail.com>
2018-09-26 14:01:08 +0000

    add js lisence

--- app/assets/javascripts/common/edges.js
+++ app/assets/javascripts/common/edges.js
@@ -1,6 +1,14 @@
 
 //= require vendor/bootstrap-tour.min.js
 
+/* ======================================================================
+ * Copyright © 2018 Imaginnection. All rights reserved.
+ 
+ * This source code or any portion thereof must not be  
+ * reproduced or used in any manner whatsoever.
+ * ======================================================================
+ */
+
 //= require edges/firebase/data.js
 //= require edges/firebase/init.js
 
