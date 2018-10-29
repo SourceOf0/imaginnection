55d0af9fa2853a7d99ca3e42f6bd60d78b3ae93d
imaginnection <imaginnection@gmail.com>
2018-10-29 06:38:24 +0000

    amend notification

--- app/controllers/notification_logs_controller.rb
+++ app/controllers/notification_logs_controller.rb
@@ -20,8 +20,9 @@ class NotificationLogsController < ApplicationController
     # エッジの通知作成
     data = params[:data].as_json
     
-    if current_user.notified_at >= Time.now.ago(1.second)
+    if current_user.notified_at >= Time.now.ago(10.second)
       render status: 400, json: { status: 400, message: 'Bad Request' }
+      return
     elsif !!data
       data.each do |gaze, edge|
         words = []
