4871b151744c180df1c429edb828993da2072c47
imaginnection <imaginnection@gmail.com>
2019-12-09 07:47:22 +0000

    amend gems

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
-//= link_tree ../images
 //= link_directory ../javascripts .js
+//= link_directory ../javascripts/common .js
+//= link_directory ../javascripts/edges .js
+//= link_directory ../javascripts/vendor .js
+
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/vendor .css
